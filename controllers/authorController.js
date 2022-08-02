07a9809392ba3e21f213e8856827fdc4a40d8a4c
Sanghak Kim <k3hppk@gmail.com>
2022-08-02 09:54:54 +0900

    0802

--- controllers/authorController.js
+++ controllers/authorController.js
@@ -1,4 +1,6 @@
 var Author = require("../models/author");
+var async = require("async");
+var Book = require("../models/book");
 
 // Display list of all Authors
 exports.author_list = function (req, res, next) {
@@ -18,7 +20,32 @@ exports.author_list = function (req, res, next) {
 
 // Display detail page for a specific Author.
 exports.author_detail = function (req, res) {
-  res.send("NOT IMPLEMENTED: Author detail: " + req.parms.id);
+  async.parallel(
+    {
+      author(callback) {
+        Author.findById(req.params.id).exec(callback);
+      },
+      author_books(callback) {
+        Book.find({ author: req.params.id }, "title summary").exec(callback);
+      },
+    },
+    function (err, results) {
+      if (err) {
+        return next(err);
+      }
+      if (results.author == null) {
+        var err = new Error("Author not found");
+        err.status = 404;
+        return next(err);
+      }
+
+      res.render("author_detail", {
+        title: "Author Detail",
+        author: results.author,
+        author_books: results.author_books,
+      });
+    }
+  );
 };
 
 // Display Author create form on GET
--- controllers/genreController.js
+++ controllers/genreController.js
@@ -1,4 +1,6 @@
 var Genre = require("../models/genre");
+var Book = require("../models/book");
+var async = require("async");
 
 // Display list of all Genre.
 exports.genre_list = function (req, res, next) {
@@ -17,8 +19,35 @@ exports.genre_list = function (req, res, next) {
 };
 
 // Display detail page for a specific Genre.
-exports.genre_detail = function (req, res) {
-  res.send("NOT IMPLEMENTED: Genre detail: " + req.params.id);
+exports.genre_detail = function (req, res, next) {
+  async.parallel(
+    {
+      genre(callback) {
+        Genre.findById(req.params.id).exec(callback);
+      },
+
+      genre_books(callback) {
+        Book.find({ genre: req.params.id }).exec(callback);
+      },
+    },
+    function (err, results) {
+      if (err) {
+        return next(err);
+      }
+      if (results.genre == null) {
+        var err = new Error("Genre not found");
+        err.status = 404;
+        return next(err);
+      }
+
+      // Successful, so render
+      res.render("genre_detail", {
+        title: "Genre Detail",
+        genre: results.genre,
+        genre_books: results.genre_books,
+      });
+    }
+  );
 };
 
 // Display Genre create form on GET.
