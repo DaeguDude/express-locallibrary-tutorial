5716284aa8de83c935a023e549018cbf16a0c583
Sanghak Kim <k3hppk@gmail.com>
2022-08-07 13:21:01 +0900

    0807 Do the challenge next time

--- controllers/authorController.js
+++ controllers/authorController.js
@@ -115,13 +115,65 @@ exports.author_create_post = [
 ];
 
 // Display Author delete form on GET.
-exports.author_delete_get = function (req, res) {
-  res.send("NOT IMPLEMENTED: Author delete GET");
+exports.author_delete_get = function (req, res, next) {
+  async.parallel(
+    {
+      author(callback) {
+        Author.findById(req.params.id).exec(callback);
+      },
+      authors_books(callback) {
+        Book.find({ author: req.params.id }).exec(callback);
+      },
+    },
+    function (err, results) {
+      if (err) {
+        return next(err);
+      }
+      if (results.author === null) {
+        res.redirect("/catalog/authors");
+      }
+
+      res.render("author_delete", {
+        title: "Delete Author",
+        author: results.author,
+        author_books: results.authors_books,
+      });
+    }
+  );
 };
 
 // Handle Author delete on POST
-exports.author_delete_post = function (req, res) {
-  res.send("NOT IMPLEMENTED: Author delte POST");
+exports.author_delete_post = function (req, res, next) {
+  async.parallel(
+    {
+      author(callback) {
+        Author.findById(req.body.authorid).exec(callback);
+      },
+      authors_books(callback) {
+        Book.find({ author: req.body.authorid }).exec(callback);
+      },
+    },
+    function (err, results) {
+      if (err) {
+        return next(err);
+      }
+      if (results.authors_books.length > 0) {
+        res.render("author_delete", {
+          title: "Delete Author",
+          author: results.author,
+          author_books: results.author_books,
+        });
+        return;
+      } else {
+        Author.findByIdAndRemove(req.body.authorid, function deleteAuthor(err) {
+          if (err) {
+            return next(err);
+          }
+          res.redirect("/catalog/authors");
+        });
+      }
+    }
+  );
 };
 
 // Display Author update form on GET.
