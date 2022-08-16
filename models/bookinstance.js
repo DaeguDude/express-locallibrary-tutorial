fa24c22ba1a4b8139e23cf6e05dbef2c73a0e75b
Sanghak Kim <k3hppk@gmail.com>
2022-08-16 14:22:45 +0900

    bookinstance update implemented

--- models/bookinstance.js
+++ models/bookinstance.js
@@ -24,6 +24,10 @@ BookInstanceSchema.virtual("due_back_formatted").get(function () {
   return DateTime.fromJSDate(this.due_back).toLocaleString(DateTime.DATE_MED);
 });
 
+BookInstanceSchema.virtual("due_back_yyyymmdd").get(function () {
+  return DateTime.fromJSDate(this.due_back).toISODate();
+});
+
 const BookInstance = mongoose.model("BookInstance", BookInstanceSchema);
 
 //Export model
