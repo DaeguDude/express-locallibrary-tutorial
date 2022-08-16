370a1075d9dff74e0779888c155128e287d42274
Sanghak Kim <k3hppk@gmail.com>
2022-08-16 14:42:46 +0900

    Update Author implemented

--- controllers/authorController.js
+++ controllers/authorController.js
@@ -205,6 +205,66 @@ exports.author_update_get = function (req, res) {
 };
 
 // Handle Author update on POST.
-exports.author_update_post = function (req, res) {
-  res.send("NOT IMPLEMENTED: Author update POST");
-};
+exports.author_update_post = [
+  body("first_name")
+    .trim()
+    .isLength({ min: 1 })
+    .escape()
+    .withMessage("First name must be specified.")
+    .isAlphanumeric()
+    .withMessage("First name has non-alphanumeric characters."),
+  body("family_name")
+    .trim()
+    .isLength({ min: 1 })
+    .escape()
+    .withMessage("Family name must be specified.")
+    .isAlphanumeric()
+    .withMessage("Family name has non-alphanumeric characters."),
+  body("date_of_birth", "Invalid date of birth")
+    .optional({ checkFalsy: true })
+    .isISO8601()
+    .toDate(),
+  body("date_of_death", "Invalid date of death")
+    .optional({ checkFalsy: true })
+    .isISO8601()
+    .toDate(),
+
+  // Create a new author object to save in the DB
+  (req, res, next) => {
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+      res.render("author_form", {
+        title: "Update Author",
+        author: req.body,
+        errors: errors.array(),
+      });
+      return;
+    } else {
+      // Create an Author object with escaped and trimmed data.
+      var author = new Author({
+        first_name: req.body.first_name,
+        family_name: req.body.family_name,
+        date_of_birth: req.body.date_of_birth,
+        date_of_death: req.body.date_of_death,
+        _id: req.params.id,
+      });
+
+      Author.findByIdAndUpdate(
+        req.params.id,
+        author,
+        {},
+        function (err, author) {
+          if (err) {
+            return next(err);
+          }
+
+          res.redirect(author.url);
+        }
+      );
+    }
+  },
+
+  // If errors - re-render the update view with errors
+  // Data from form is valid. Update the bookinstnace
+];
